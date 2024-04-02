db89c1eef540b103ca84a8a46b7be73e12ba2ced
Jessica Silva <jessica.akyla@gmail.com>
2024-04-02 19:08:43 -0300

    Exercício usando while

---
+++ src/Cadastro.java
@@ -0,0 +1,27 @@
+import                                                                          java.util.Scanner;
+
+public class Cadastro {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        final String usuario = "Jessica";
+        final int senha = 1234;
+        boolean exibirTelaLogin = true;
+        while (exibirTelaLogin){
+            System.out.println("Acesso somente com nome do usuário e senha!");
+            System.out.println("Login do Usuário");
+            String usuarioDigitado = input.nextLine();
+            System.out.println("Senha do Usuário");
+            int senhaDigitada = input.nextInt();
+            input.nextLine();
+            if (usuarioDigitado.equals(usuario) && senhaDigitada == senha){
+                System.out.println("Acesso Liberado!");
+                exibirTelaLogin = false;
+                break;
+            }
+            System.out.println("Acesso Negado!");
+
+        }
+        System.out.println("Programa finalizado!");
+        input.close();
+    }
+}
